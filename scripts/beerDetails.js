bad2d52c3d7b0dab5dfed9d6c39142ef378e0980
gjogri <gorgivasilev@yahoo.com>
2023-03-15 10:58:18 +0100

    relocationToBeerDetailsUrl

--- scripts/beerDetails.js
+++ scripts/beerDetails.js
@@ -1,9 +1,22 @@
 let beerDetails = document.getElementById("beerDetails");
-let options = {
-  method: "GET",
-  url: "https://api.punkapi.com/v2/beers/random",
-};
+const id = window.location.href.split("id=");
+if (window.location.href.includes("id")) {
+  let optionsWithId = {
+    method: "GET",
+    url: `https://api.punkapi.com/v2/beers/${id[1]}`,
+  };
+  console.log(optionsWithId);
 
-axios.request(options).then((singleBeer) => {
-  beerDeatils(singleBeer);
-});
+  axios.request(optionsWithId).then((singleBeer) => {
+    beerDeatils(singleBeer);
+  });
+} else {
+  let options = {
+    method: "GET",
+    url: "https://api.punkapi.com/v2/beers/random",
+  };
+  // console.log(singleBeer);
+  axios.request(options).then((singleBeer) => {
+    beerDeatils(singleBeer);
+  });
+}
